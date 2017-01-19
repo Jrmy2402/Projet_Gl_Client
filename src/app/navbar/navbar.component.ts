893d163ddf019cb3f6a008c56b62e7efd44fe508
Jérémy Spriet <jeremy.spriet@gmail.com>
2017-01-19 14:32:33 +0100

    component dialog

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MdDialog, MdDialogRef } from '@angular/material';
+import { DialogInscriptionComponent } from '../dialog-inscription/dialog-inscription.component';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  // dialogRef: MdDialogRef<DialogInscriptionComponent>;
+
+  constructor(public dialog: MdDialog) { }
 
   ngOnInit() {
+  }
+
+  openDialogInscription() {
+    let dialogRef = this.dialog.open(DialogInscriptionComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      // this.selectedOption = result;
+    });
   }
 
 }
